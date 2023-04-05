a15a1504ac3064f84e7ce42c7a9ad0eb71c066b4
dhanasekaran12 <127470374+dhanasekaran12@users.noreply.github.com>
2023-04-05 18:51:44 +0530

    Add files via upload

---
+++ taskd6.js
@@ -0,0 +1,40 @@
+//person class
+class Person {
+    constructor(name,age) {
+      this.name = name;
+      this.age = age;
+    }
+    detail()
+    {
+        return "Name  :"+ this.name+"   Age  :"+this.age;
+
+    }
+
+  
+}  
+  const per1 = new Person("Dhana",19);
+console.log (per1.detail());
+  
+
+// //calculate uberprice
+// class Uberprice{
+//     constructor(distance,time)
+// {
+//     this.distance=distance;
+//     this.time=time;
+//     this.fare=3;
+//     this.minfare=0.5;
+//     this.kmfare=2;
+
+// }
+// calculation()
+// {
+//     let timecost =   this.time * this.minfare ;
+//     let distcost = this.distance * this.kmfare ;
+//     let uberfare=this.fare+timecost+distcost;
+//     return uberfare;
+// }
+
+// }
+// let ride= new Uberprice(10,120);
+// console.log(ride.calculation());
