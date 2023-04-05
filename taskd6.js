7dfe1947b4b98c60ec7212bd4b23eb9e53ec943b
dhanasekaran12 <127470374+dhanasekaran12@users.noreply.github.com>
2023-04-05 18:53:10 +0530

    Update taskd6.js

--- taskd6.js
+++ taskd6.js
@@ -16,25 +16,25 @@ class Person {
 console.log (per1.detail());
   
 
-// //calculate uberprice
-// class Uberprice{
-//     constructor(distance,time)
-// {
-//     this.distance=distance;
-//     this.time=time;
-//     this.fare=3;
-//     this.minfare=0.5;
-//     this.kmfare=2;
+ //calculate uberprice
+ class Uberprice{
+     constructor(distance,time)
+ {
+     this.distance=distance;
+     this.time=time;
+     this.fare=3;
+     this.minfare=0.5;
+     this.kmfare=2;
 
-// }
-// calculation()
-// {
-//     let timecost =   this.time * this.minfare ;
-//     let distcost = this.distance * this.kmfare ;
-//     let uberfare=this.fare+timecost+distcost;
-//     return uberfare;
-// }
+ }
+ calculation()
+ {
+     let timecost =   this.time * this.minfare ;
+     let distcost = this.distance * this.kmfare ;
+     let uberfare=this.fare+timecost+distcost;
+     return uberfare;
+ }
 
-// }
-// let ride= new Uberprice(10,120);
-// console.log(ride.calculation());
+ }
+ let ride= new Uberprice(10,120);
+console.log(ride.calculation());
